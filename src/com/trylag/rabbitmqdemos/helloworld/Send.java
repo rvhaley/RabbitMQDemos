787c4312d062d782ac5e58f7f6c35e45a3136d23
Richard Haley III <admin@tryla-g.com>
2014-11-14 06:06:42 +0100

    Completed the Send class for the Hello World example.

--- src/com/trylag/rabbitmqdemos/helloworld/Send.java
+++ src/com/trylag/rabbitmqdemos/helloworld/Send.java
@@ -13,17 +13,33 @@ import static com.trylag.rabbitmqdemos.constants.Constants.*;
  */
 public class Send {
 
+    private static final String QUEUE_NAME = "rh11_hello";
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
+
+        // Create a connection factory with connection details
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost(CONNECTION_HOST);
         factory.setPort(CONNECTION_PORT);
         factory.setUsername(CONNECTION_USERNAME);
         factory.setPassword(CONNECTION_PASSWORD);
-        
         Connection connection = factory.newConnection();
+
+        // Create a channel from the connection
         Channel channel = connection.createChannel();
+
+        // Declare a queue to publish to
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        // Publish to the queue using the default exchange
+        channel.basicPublish("", QUEUE_NAME, null, HELLO_WORLD.getBytes());
+        System.out.println(" [X] Sent : '" + HELLO_WORLD + "'");
+
+        // Close the channel and connection
+        channel.close();
+        connection.close();
     }
 }
