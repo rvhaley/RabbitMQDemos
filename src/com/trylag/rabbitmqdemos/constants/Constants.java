7777e37b77d4fd1f1b6e266373ea29ac4b09055d
Richard Haley III <admin@tryla-g.com>
2014-11-14 05:57:15 +0100

    Created constants class for static import, and created Send class.

---
+++ src/com/trylag/rabbitmqdemos/constants/Constants.java
@@ -0,0 +1,21 @@
+package com.trylag.rabbitmqdemos.constants;
+
+/**
+ *
+ * @author Richard Haley III
+ */
+public class Constants {
+
+    /*
+     Connection details
+     */
+    public static final String CONNECTION_HOST = "datdb.cphbusiness.dk";
+    public static final int CONNECTION_PORT = 5672;
+    public static final String CONNECTION_USERNAME = "student";
+    public static final String CONNECTION_PASSWORD = "cph";
+
+    /*
+     Hello world
+     */
+    public static final String HELLO_WORLD = "Hello, World!";
+}
--- src/com/trylag/rabbitmqdemos/helloworld/Send.java
+++ src/com/trylag/rabbitmqdemos/helloworld/Send.java
@@ -1,12 +1,29 @@
 package com.trylag.rabbitmqdemos.helloworld;
 
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import java.io.IOException;
+
+import static com.trylag.rabbitmqdemos.constants.Constants.*;
+
 /**
  *
  * @author Richard Haley III
  */
 public class Send {
 
-    public static void main(String[] args) {
-
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(CONNECTION_HOST);
+        factory.setPort(CONNECTION_PORT);
+        factory.setUsername(CONNECTION_USERNAME);
+        factory.setPassword(CONNECTION_PASSWORD);
+        
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
     }
 }
